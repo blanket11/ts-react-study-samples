b0ebf4f4d98a388912015761e4732f926f718e2e
Blanket11 <bkweb11k@gmail.com>
2023-08-22 18:09:44 +0900

    study03

---
+++ src/components/Organisms/Study03/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Study03: React.FC = () => {
+  return (
+    <p>親は必ず１つにするひつようがあります。</p>
+    <p>このように複数タグが並列の場合親が必要です。</p>
+    <p>1つ目は、React.Fragment を使って問題を解決してみます。</p>
+    <p>２つ目は、React.Fragment の省略形を使って問題解決します。</p>
+  );
+};
+
+export default Study03;
