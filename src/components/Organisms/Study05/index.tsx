5e3d2b22871e8e063077fb15f7473f4c8ca54a3d
Blanket11 <bkweb11k@gmail.com>
2023-08-23 10:17:01 +0900

    if の練習

---
+++ src/components/Organisms/Study05/index.tsx
@@ -0,0 +1,24 @@
+import Spacer from "@components/Atoms/Spacer";
+import Title from "@components/Atoms/Title";
+
+const Study05: React.FC = () => {
+  const name = "山田 太郎";
+  const guestFlag = true;
+  const isLogin = false;
+  return (
+    <div>
+      <Title>条件付きレンダー</Title>
+      <p>guestFlag が trueなら 様を表示させましょう！</p>
+      <p>
+        名前: {name} <small>様</small>
+      </p>
+      <Spacer />
+      <Title>三項演算子</Title>
+      <p>isLogin が true falseでテキストを出し分けましょう！</p>
+      <p>ようこそ{name}さん。</p>
+      <p>ゲストとして閲覧中です。</p>
+    </div>
+  );
+};
+
+export default Study05;
