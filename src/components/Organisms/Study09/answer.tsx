c84d84e1c0a980be1175c84d4869561e001221f7
Blanket11 <bkweb11k@gmail.com>
2023-08-23 17:29:41 +0900

    study09回答

---
+++ src/components/Organisms/Study09/answer.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import Title from "@components/Atoms/Title";
+import Spacer from "@components/Atoms/Spacer";
+import Button from "@components/Atoms/Button";
+
+const Study09 = () => {
+  const [counter, setCounter] = useState(0);
+
+  // 問題１
+  useEffect(() => {
+    console.log("コンポーネントが表示されたら実行");
+  }, []);
+
+  // 問題２
+  useEffect(() => {
+    console.log("counterが変更されたら実行");
+  }, [counter]);
+
+  const countUp = () => {
+    setCounter(counter + 1);
+  };
+  return (
+    <div>
+      <Title>useEffectを知ろう</Title>
+      <p>console.logを確認</p>
+      <Spacer />
+      <p>カウント：{counter}</p>
+      <Spacer />
+      <button onClick={countUp}>
+        <Button size="sm">+1する</Button>
+      </button>
+    </div>
+  );
+};
+
+export default Study09;
