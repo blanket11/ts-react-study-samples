fa9305b73b169c3997ea9f858f5880cf0c93a474
Blanket11 <bkweb11k@gmail.com>
2023-08-23 17:10:58 +0900

    Study08の回答

---
+++ src/components/Organisms/Study08/answer.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Button from "@components/Atoms/Button";
+import Spacer from "@components/Atoms/Spacer";
+import Title from "@components/Atoms/Title";
+
+const Study08 = () => {
+  const [counter, setCounter] = useState(0);
+  const countUp = () => {
+    setCounter(counter + 1);
+  };
+  return (
+    <div>
+      <Title>useStateを使ってカウントアップ</Title>
+      <p>
+        useStateで counter と setCounter
+        を作りボタンを押したら数字がカウントアップするようにする。
+      </p>
+      {/* 1. useStateの読み込み */}
+      {/* 2. useState を使い counter と setCounter を用意 */}
+      {/* 3. countUp 関数を用意 */}
+      {/* 4. buttonタグでButtonタグを囲い onClick を設定 */}
+      {/* 5. Astroの仕様で一部ブラウザレンダリングにするため client:load を設定する */}
+      <Spacer />
+      <p>カウント：{counter}</p>
+      <Spacer />
+      <button onClick={countUp}>
+        <Button size="sm">+1する</Button>
+      </button>
+    </div>
+  );
+};
+
+export default Study08;
