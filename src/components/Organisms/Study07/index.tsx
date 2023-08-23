3edddffb1e622bfd92e8d3938e081332d3575a7a
Blanket11 <bkweb11k@gmail.com>
2023-08-23 11:20:15 +0900

    useStateのレシピ追記

--- src/components/Organisms/Study07/index.tsx
+++ src/components/Organisms/Study07/index.tsx
@@ -10,6 +10,11 @@ const Study07 = () => {
         useStateで counter と setCounter
         を作りボタンを押したら数字がカウントアップするようにする。
       </p>
+      {/* 1. useStateの読み込み */}
+      {/* 2. useState を使い counter と setCounter を用意 */}
+      {/* 3. countUp 関数を用意 */}
+      {/* 4. buttonタグでButtonタグを囲い onClick を設定 */}
+      {/* 5. Astroの仕様で一部ブラウザレンダリングにするため client:load を設定する */}
       <Spacer />
       <p>カウント：0</p>
       <Spacer />
